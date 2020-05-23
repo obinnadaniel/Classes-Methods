2239aeeeab9eb630bffac4099fd58843f69036de
Obinna Daniel <obinnadaniel30@yahoo.com>
2020-05-23 12:23:14 +0100

    initial commit

---
+++ Classes-Methods/Airplane.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Methods
+{
+    class Airplane
+    {
+        int wings, wheels, engines;
+
+        public void Run()
+        {
+            this.wheels = 6;
+            Console.WriteLine("Running");
+        }
+        public void Fly()
+        {
+            this.wings = 2;
+            Console.WriteLine("Flying");
+
+        }
+        public void Powerplant()
+        {
+            this.engines = 4;
+            Console.WriteLine("Power...");
+        }
+    }
+    class Fighterjet : Airplane
+    {
+
+    }
+}
---
+++ Classes-Methods/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fighterjet f16 = new Fighterjet();
+            f16.Fly();
+            Console.ReadLine();
+        }
+    }
+}
